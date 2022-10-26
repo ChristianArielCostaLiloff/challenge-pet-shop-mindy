58a9d2ababd63bdd6fb180c58b695c8701985b40
ChristianArielCostaLiloff <private.c.costa@gmail.com>
2022-10-26 16:00:17 -0300

    Crear cartas(demo) por tipo medicamento con datos de la API

---
+++ assets/js/farmacia.js
@@ -0,0 +1,50 @@
+const navSearch = document.getElementById("nav-search-content");
+const container = document.getElementById("container");
+
+showContent();
+async function showContent() {
+  let data;
+  try {
+    data = await (
+      await fetch("https://apipetshop.herokuapp.com/api/articulos")
+    ).json();
+  } catch (error) {
+    console.log("Error with API");
+  }
+  dataPharmacyProducts = data.response.filter(
+    (product) => product.tipo == "Medicamento"
+  );
+  dataPharmacyProducts.forEach(createProduct);
+  navSearch.addEventListener("input", function () {
+    let minPrice = document.getElementById("min-price").value;
+    let maxPrice = document.getElementById("max-price").value;
+    let textSearch = document.getElementById("text-search").value.toLowerCase();
+    let sortedProducts = dataPharmacyProducts.filter(
+      (product) =>
+        minPrice <= product.precio &&
+        product.precio <= maxPrice &&
+        product.nombre.includes(textSearch)
+    );
+    clearHtml(container);
+    sortedProducts.forEach(createProduct);
+    //0 matches
+    if (sortedProducts.length < 1) {
+      container.innerHTML = "<h2>No elements to display</h2>";
+    }
+  });
+}
+function clearHtml(domElement) {
+  domElement.innerHTML = "";
+}
+function createProduct(product) {
+  container.innerHTML += `
+    <div class="card" style="width: 18rem;">
+        <img src="${product.imagen}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${product.nombre}</h5>
+            <p class="card-text">${product.descripcion}</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+    `;
+}
