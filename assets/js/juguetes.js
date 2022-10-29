343867327d7531da2b2032da51d0a0b525c97d0e
ChristianArielCostaLiloff <private.c.costa@gmail.com>
2022-10-29 17:04:40 -0300

    fixed search uppercase

--- assets/js/juguetes.js
+++ assets/js/juguetes.js
@@ -13,10 +13,12 @@ async function showContent() {
   } catch (error) {
     console.log("Error with API");
   }
+  console.log(data);
   let dataToysProducts = data.response.filter(
     (product) => product.tipo == "Juguete"
   );
   dataToysProducts.forEach(createProduct);
+
   navSearch.addEventListener("input", function () {
     let sort = document.getElementById("sort").value;
     let minPrice = document.getElementById("min-price").value;
@@ -40,12 +42,14 @@ async function showContent() {
         a.precio > b.precio ? a : b
       ).precio;
     }
+
     let sortedProducts = dataToysProducts.filter(
       (product) =>
         minPrice <= product.precio &&
         product.precio <= maxPrice &&
-        product.nombre.includes(textSearch)
+        product.nombre.toLowerCase().includes(textSearch)
     );
+
     clearHtml(container);
     sortedProducts.forEach(createProduct);
     //0 matches
@@ -58,20 +62,16 @@ function clearHtml(domElement) {
   domElement.innerHTML = "";
 }
 function createProduct(product) {
-  let quantity = 0,
-    btnStyle, textStyle, text, style;
+  let textStyle, text, style;
   if (cart.filter((e) => e.id == product._id).length > 0) {
-    btnStyle = "btn-buy btn-buy--clicked";
     style = "card-stats--background-red";
+  } 
+  if (product.stock < 4) {
+    textStyle = "date--red";
+    text = "¡Ultimas " + product.stock + " Unidades Disponibles!";
   } else {
-    btnStyle = "btn-buy";
-  }
-  if (product.stock<4) {
-    textStyle = "date--red"
-    text= "¡Ultimas " + product.stock + " Unidades Disponibles!"
-  } else {
-    textStyle = "date--green"
-    text= product.stock + " Unidades restantes"
+    textStyle = "date--green";
+    text = product.stock + " Unidades restantes";
   }
   container.innerHTML += `
     <div class="card mt-3">
@@ -93,7 +93,7 @@ function createProduct(product) {
                <input type="number" name="text" class="card__cantidad" placeholder="0" min="0" max="${product.stock}" id="input-${product._id}">
             </div>
             <div class="stat">
-              <input class="${btnStyle}" type="image" src="../assets/img/compras.png" onclick="handleCart('${product._id}')" id="btn-${product._id}">
+              <input class="btn-buy" type="image" src="../assets/img/compras.png" onclick="handleCart('${product._id}')" id="btn-${product._id}">
             </div>
           </div>
         </div>
@@ -102,13 +102,13 @@ function createProduct(product) {
 
 function handleCart(id) {
   const btn = document.getElementById("btn-" + id);
-  const inputQuantity = document.getElementById("input-quantity-"+id);
+  const inputQuantity = document.getElementById("input-quantity-" + id);
   const quantity = document.getElementById("input-" + id).value;
   if (cart.filter((e) => e.id == id).length > 0) {
     cart = cart.filter((product) => product.id != id);
     localStorage.setItem("cart", JSON.stringify(cart));
     btn.classList.remove("btn-buy--clicked");
-    inputQuantity.classList.remove("card-stats--background-red")
+    inputQuantity.classList.remove("card-stats--background-red");
     //btn.classList.add("btn-primary");
   } else {
     if (quantity > 0) {
@@ -116,7 +116,7 @@ function handleCart(id) {
       localStorage.setItem("cart", JSON.stringify(cart));
       //btn.classList.remove("btn-primary");
       btn.classList.add("btn-buy--clicked");
-      inputQuantity.classList.add("card-stats--background-red")
+      inputQuantity.classList.add("card-stats--background-red");
     } else {
       alert("Ingrese un numero mayor a 0");
     }
